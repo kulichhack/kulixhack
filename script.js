0992998ee8055f57369d8edc66c3e738ebfdb7d6
kulichhack <artemkulicenkov31@gmail.com>
2025-05-21 11:39:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function najatie() {
 let nadpis = document.getElementById("nadpis").innerHTML
-if nadpis === "" {
+if (nadpis === "") {
 nadpis ="Здесь текст, который появится после нажатия кнопки"
 }
 else {
